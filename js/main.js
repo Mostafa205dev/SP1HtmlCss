a550338a5ff5c6432f90b55da72954ac2ad41145
Mostafa205dev <seso63412@gmail.com>
2025-04-29 14:45:20 +0200

    done time events

--- js/main.js
+++ js/main.js
@@ -1,37 +1,20 @@
-/*
- ** Countdown Timer
- ** Video URL: https://www.youtube.com/watch?v=eFsiOTJrrE8
- */
 
-// The End Of The Year Date
-// 1000 milliseconds = 1 Second
+// latest events date
+let datenow = new Date();
+let birthday = new Date("Apr 29,25");
+let diff = datenow - birthday;
 
-let countDownDate = new Date("Dec 31, 2022 23:59:59").getTime();
-// console.log(countDownDate);
+const dayss = document.querySelector(".events .time .days");
+const hourss = document.querySelector(".events .time .hours");
+const Minutess = document.querySelector(".events .time .minutes");
+const Secondss = document.querySelector(".events .time .seconds");
 
-let counter = setInterval(() => {
-  // Get Date Now
-  let dateNow = new Date().getTime();
+dayss.innerHTML =   `${ Math.floor(diff /1000 /60 /60 /24)}`;
+hourss.innerHTML =  `${ Math.floor(diff /1000 /60 /60)    }`;
+Minutess.innerHTML =`${ Math.floor(diff /1000 /60)        }`;
+Secondss.innerHTML =`${ Math.floor(diff /1000)            }`;
 
-  // Find The Date Difference Between Now And Countdown Date
-  let dateDiff = countDownDate - dateNow;
 
-  // Get Time Units
-  // let days = Math.floor(dateDiff / 1000 / 60 / 60 / 24);
-  let days = Math.floor(dateDiff / (1000 * 60 * 60 * 24));
-  let hours = Math.floor((dateDiff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  let minutes = Math.floor((dateDiff % (1000 * 60 * 60)) / (1000 * 60));
-  let seconds = Math.floor((dateDiff % (1000 * 60)) / 1000);
-
-  document.querySelector(".days").innerHTML = days < 10 ? `0${days}` : days;
-  document.querySelector(".hours").innerHTML = hours < 10 ? `0${hours}` : hours;
-  document.querySelector(".minutes").innerHTML = minutes < 10 ? `0${minutes}` : minutes;
-  document.querySelector(".seconds").innerHTML = seconds < 10 ? `0${seconds}` : seconds;
-
-  if (dateDiff < 0) {
-    clearInterval(counter);
-  }
-}, 1000);
 
 /*
  ** Animate Width On Scrolling
@@ -53,7 +36,7 @@ let started = false; // Function Started ? No
 let scrollup = document.getElementById("button");
 let scrolldown = document.getElementById("scrolldown");
 scrolldown.onclick = function(){
-  window.scrollTo(0,1000);
+  window.scrollTo(0,100000);
   
 }
 window.onscroll = function () {
@@ -92,5 +75,8 @@ function startCount(el) {
     }
   }, 2000 / goal);
 }
+
+
+
 
 
